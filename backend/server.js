3cbda26bcdb731084c0609a8f912a32697a3c269
Ayush <ayushkmishra332@gmail.com>
2025-09-21 03:37:50 +0530

    fix cors error

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://flowcareer.netlify.app", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
